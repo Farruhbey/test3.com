6343d951acdefd4a7c12629f1c66157aec5a502d
Farruhbey <farruhbeyrixsiboyev05@gmail.com>
2021-11-26 18:05:55 +0500

    d

--- academiya/settings.py
+++ academiya/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-0sf$2=2cwpy%4lf*-z&qtztcs=*))&a!rzfnkk+#o9(8-l0b%b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1","test3.com","81.95.232.205"]
 
 
 # Application definition
